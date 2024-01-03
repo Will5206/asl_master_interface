28c7fc415bef7f549b856e21b2640f283052fa91
Will Silver Wagman <wagmanwill@gmail.com>
2024-01-03 15:25:55 -0800

    Initial commit of the interface draft

---
+++ src/components/SearchResult.vue
@@ -0,0 +1,136 @@
+<template>
+    <div id="search-result">
+      <div class="img-container" :class="{ loaded: imgHasLoaded }">
+        <img :src="imgSrc" id="sign-img" alt="" />
+  
+        <div class="expanded-info" v-show="showInfo">
+          <p><strong>Hands: </strong>{{ hands != '' ? hands : 'Unknown' }}</p>
+          <p>
+            <strong>Handshape: </strong
+            >{{ handshape != '' ? handshape : 'Unknown' }}
+          </p>
+          <p>
+            <strong>Location: </strong>{{ location != '' ? location : 'Unknown' }}
+          </p>
+          <p>
+            <strong>Movement: </strong>{{ movement != '' ? movement : 'Unknown' }}
+          </p>
+        </div>
+      </div>
+      <div class="sign-description">
+        <a class="info" @mouseover="showInfo = true" @mouseout="showInfo = false"
+          ><span
+        /></a>
+        <p class="sign-name">{{ formattedSign }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'SearchResult',
+    data() {
+      return {
+        showInfo: false,
+        imgHasLoaded: false,
+      };
+    },
+    methods: {},
+    mounted() {},
+    computed: {
+      formattedSign: function () {
+        return this.sign.charAt(0).toUpperCase() + this.sign.slice(1);
+      },
+    },
+    props: {
+      sign: {
+        type: String,
+        default: '',
+      },
+      imgSrc: {
+        type: String,
+        default: '',
+      },
+      hands: {
+        type: String,
+        default: '',
+      },
+      handshape: {
+        type: String,
+        default: '',
+      },
+      location: {
+        type: String,
+        default: '',
+      },
+      movement: {
+        type: String,
+        default: '',
+      },
+    },
+  };
+  </script>
+  
+  <style lang="scss" scoped>
+  @import '@/assets/styles/_variables.scss';
+  @import '@/assets/styles/_mixins.scss';
+  
+  #search-result {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+  }
+  
+  .img-container {
+    position: relative;
+    background: rgba(0, 0, 0, 0.1);
+    aspect-ratio: 4/3;
+    img {
+      width: 100%;
+    }
+  }
+  
+  .sign-description {
+    display: grid;
+    grid-template-columns: min-content auto;
+    gap: 0.5rem;
+  }
+  
+  .expanded-info {
+    position: absolute;
+    inset: 0.25rem;
+    border-radius: 0.25rem;
+    background-color: $transp-light-accent;
+    backdrop-filter: blur(8px);
+    padding: 0.5rem;
+  
+    p {
+      @include fs(-1);
+      line-height: 1.1;
+  
+      & + p {
+        margin-top: 0.5rem;
+      }
+      strong {
+        font-weight: 700;
+      }
+    }
+  }
+  
+  .info {
+    span {
+      display: inline-block;
+      margin-top: 0.2rem;
+      width: 1rem;
+      height: 1rem;
+      cursor: help;
+      background-size: contain;
+      background-image: url(info-circle-icon(#6363e7));
+    }
+  }
+  
+  .sign-name {
+    @include fs(-1);
+    font-weight: 400;
+  }
+  </style>
---
+++ src/components/VideoControls.vue
@@ -0,0 +1,207 @@
+<template>
+    <div id="video-controls">
+      <button
+        id="general-play"
+        @click="$emit('general-play')"
+        class="icon-bg"
+        :class="generalPlayClass"
+      >
+        &nbsp;
+      </button>
+      <button
+        id="selection-play"
+        @click="$emit('selection-play')"
+        :class="{ pause: playingSelection, play: !playingSelection }"
+      >
+        Play selection
+      </button>
+
+      <button @click="uploadVideo" class="upload-button">
+        <svg fill="#000000" height="24px" width="24px" viewBox="0 0 330 330" xmlns="http://www.w3.org/2000/svg">
+          <path d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394
+            l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393
+            C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z"/>
+        </svg>
+        Upload Video
+      </button>
+
+      <p id="elapsed-time">
+        <span class="hours" :class="{ 'less-than-an-hour': duration < 3600 }">{{
+          formattedTime.hours
+        }}</span
+        ><span class="colon">:</span
+        ><span class="minutes" :class="{ 'less-than-a-minute': duration < 60 }">{{
+          formattedTime.minutes
+        }}</span
+        ><span class="colon">:</span
+        ><span class="seconds">{{ formattedTime.seconds }}</span
+        ><span class="period">.</span
+        ><span class="deciseconds">{{ formattedTime.deciseconds }}</span>
+      </p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'VideoControls',
+    props: {
+      playing: {
+        type: Boolean,
+        default: false,
+      },
+      playingSelection: {
+        type: Boolean,
+        default: false,
+      },
+      currentTime: {
+        type: Number,
+        default: 0,
+      },
+      duration: {
+        type: Number,
+        default: 0,
+      },
+    },
+    computed: {
+      generalPlayClass: function () {
+        if (!this.playing) {
+          if (this.currentTime == this.duration) {
+            return 'reset';
+          }
+          return 'play';
+        }
+        return 'pause';
+      },
+      formattedTime: function () {
+        let t = new Date(0);
+        t.setSeconds(this.currentTime);
+        let s = t.toISOString();
+        return {
+          hours: s.substr(11, 2),
+          minutes: s.substr(14, 2),
+          seconds: s.substr(17, 2),
+          deciseconds: (this.currentTime % 1).toFixed(2).substring(2, 3),
+        };
+      },
+    },
+    methods: {
+      // ... other methods ...
+      uploadVideo() {
+        this.$emit('upload-video');
+      },
+    },
+  };
+  </script>
+  
+  <style lang="scss" scoped>
+  @import '@/assets/styles/_variables.scss';
+  @import '@/assets/styles/_mixins.scss';
+  
+  #video-controls {
+    display: flex;
+    align-items: center; // Align items vertically
+    justify-content: space-between; // Space out items
+    background-color: black;
+    padding: 0 12px;
+    width: 100%;
+    height: 60px
+  }
+
+  
+  #elapsed-time {
+  @include fs(1);
+  text-align: right;
+  color: white;
+  // Flex item alignment is handled by the container
+  span {
+    width: 2.5ch;
+    display: inline-block;
+    text-align: center;
+    &.colon, &.period {
+      width: 0.4ch;
+    }
+    &.deciseconds {
+      width: 1.25ch;
+    }
+    &.colon, &.less-than-an-hour, &.less-than-a-minute {
+      color: #666;
+    }
+  }
+}
+  
+  button {
+    border: none;
+    padding: 0.25rem 1rem;
+    border-radius: 0.25rem;
+    border: 1px solid transparent;
+    color: white;
+    cursor: pointer;
+    user-select: none;
+    &#general-play {
+      background-color: #666;
+      padding-left: 2.25rem;
+      padding-right: 2.25rem;
+      &.play {
+        background-image: url(play-icon());
+        &:hover {
+          background-image: url(play-icon(#666));
+        }
+      }
+      &.pause {
+        background-image: url(pause-icon());
+        &:hover {
+          background-image: url(pause-icon(#666));
+        }
+      }
+      &.reset {
+        background-image: url(reset-icon());
+        &:hover {
+          background-image: url(reset-icon(#666));
+        }
+      }
+      &:hover {
+        background-color: #fff;
+        color: #333;
+      }
+    }
+    &#selection-play {
+      background-color: #333;
+      border-color: mix(#333, $accent);
+      color: $accent;
+      &:hover {
+        background-color: $accent;
+        border-color: $accent;
+        color: #333;
+      }
+    }
+  }
+  .upload-button {
+  padding: 0.15rem;
+  font-size: 0.85rem;
+  color: black; 
+  height: 34px;
+
+  svg {
+    height: 26px;
+    width: 20px;
+  }
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 5px; // Space between icon and text
+
+  // Explicitly control the width and remove extra space
+  width: fit-content;
+  min-width: 0;
+
+  // Remove any extra space around the content
+  white-space: nowrap;
+}
+  .icon-bg {
+    background-repeat: no-repeat;
+    background-size: 1rem;
+    background-position-y: center;
+    background-position-x: center;
+  }
+  </style>
